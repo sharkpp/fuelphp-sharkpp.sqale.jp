f44f5e5ac4b07119a99e314b4880daba41ccb4d7
sharkpp <webmaster@sharkpp.net>
2013-03-16 15:26:51 +0000

    test
    git-svn-id: http://nasserver:888/repos/main/www/fuelphp-sharkpp.sqale.jp@2414 fd21481f-4f5f-b64a-a7cc-914020ce4b36

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -51,6 +51,7 @@
 				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
 				<p>This view can be found at <code>APPPATH/views/welcome/index.php</code>.</p>
 				<p>You can modify these files to get your application started quickly.</p>
+				<p>Fuel::$env = <?php echo Fuel::$env; ?></p>
 			</div>
 			<div class="span4">
 				<h2>Learn</h2>
